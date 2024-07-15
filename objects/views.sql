d339eff37b7e4b55fca1c4df1c03f492f706c1cb
Martin Mas de Ayala <132627081+MartinMasde@users.noreply.github.com>
2024-07-15 19:09:20 -0300

    Update views.sql
    Creacion vista ReservasCliente

--- objects/views.sql
+++ objects/views.sql
@@ -1,1 +1,24 @@
--- Crear las vistas para el proyecto
+USE agencia_viajes;
+
+-- Vista para mostrar las Reservas de Viajes de un CLiente
+-- Dado el nombre de un cliente se mostraran las reservas que dicho cliente tiene hechas ordenas por fechas de manera ascendente
+CREATE VIEW ReservasCliente AS
+SELECT 
+    CONCAT (Cliente.Nombre, ' ', Cliente.Apellido) AS NombreCompleto,
+    Cliente.Direccion,
+    Cliente.Telefono,
+    Cliente.Email,
+    Cliente.TipoCliente,
+    Reserva.FechaReserva,
+	  Viaje.Destino,
+    Viaje.FechaSalida,
+    Viaje.FechaRegreso,
+    Viaje.Descripcion AS DescripcionViaje,
+    Viaje.Precio
+FROM 
+    Cliente
+JOIN 
+    Reserva ON Cliente.idCliente = Reserva.idCliente
+JOIN 
+	Viaje ON Reserva.idViaje = Viaje.idViaje
+ORDER BY Reserva.FechaReserva ASC;
