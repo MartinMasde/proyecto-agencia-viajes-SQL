c19498d29459e87636649f00bbcad4195b6649c9
Martin Mas de Ayala <132627081+MartinMasde@users.noreply.github.com>
2024-07-15 20:22:27 -0300

    Update views.sql
    Creacion vista DestinoConMasReservas

--- objects/views.sql
+++ objects/views.sql
@@ -1,24 +1,39 @@
 USE agencia_viajes;
 
--- Vista para mostrar las Reservas de Viajes de un CLiente
+-- Vista para mostrar las Reservas de Viajes de un Cliente
 -- Dado el nombre de un cliente se mostraran las reservas que dicho cliente tiene hechas ordenas por fechas de manera ascendente
-CREATE VIEW ReservasCliente AS
+CREATE VIEW 
+	ReservasCliente AS
 SELECT 
-    CONCAT (Cliente.Nombre, ' ', Cliente.Apellido) AS NombreCompleto,
-    Cliente.Direccion,
-    Cliente.Telefono,
-    Cliente.Email,
-    Cliente.TipoCliente,
-    Reserva.FechaReserva,
-	  Viaje.Destino,
-    Viaje.FechaSalida,
-    Viaje.FechaRegreso,
-    Viaje.Descripcion AS DescripcionViaje,
-    Viaje.Precio
+	CONCAT (C.Nombre, ' ', C.Apellido) AS NombreCompleto,
+	C.Direccion,
+	C.Telefono,
+	C.Email,
+	C.TipoCliente,
+	R.FechaReserva,
+	V.Destino,
+	V.FechaSalida,
+	V.FechaRegreso,
+	V.Descripcion AS DescripcionViaje,
+	V.Precio
 FROM 
-    Cliente
-JOIN 
-    Reserva ON Cliente.idCliente = Reserva.idCliente
-JOIN 
-	Viaje ON Reserva.idViaje = Viaje.idViaje
-ORDER BY Reserva.FechaReserva ASC;
+	Cliente C
+	JOIN Reserva R ON C.idCliente = R.idCliente
+	JOIN Viaje V ON R.idViaje = V.idViaje
+ORDER BY 
+	R.FechaReserva ASC;
+
+-- Vista para los Destinos de Viaje con mas Reservas
+-- Esta vista mostrara los destinos de viaje con mas cantidad de reservas hechas
+CREATE VIEW 
+	DestinoConMasReservas AS
+SELECT 
+	V.Destino,
+	COUNT(R.idReserva) AS TotalReservas
+FROM 
+	Viaje V
+	JOIN Reserva R ON V.idViaje = R.idViaje
+GROUP BY 
+	V.Destino
+ORDER BY 
+	TotalReservas DESC;
