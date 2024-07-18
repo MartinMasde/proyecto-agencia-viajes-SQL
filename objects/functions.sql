c8a63ee1dc010809183b7bdb057b07e614810460
Martin Mas de Ayala <132627081+MartinMasde@users.noreply.github.com>
2024-07-18 18:55:14 -0300

    Update functions.sql
    Primera funcion creada reservas_empleado_periodo

--- objects/functions.sql
+++ objects/functions.sql
@@ -1,1 +1,24 @@
+USE agencia_viajes;
 
+DROP FUNCTION IF EXISTS reservas_empleado_periodo;
+
+-- Funcion para contar las reservas de un empleado en un intervalo de tiempo:
+
+DELIMITER //
+
+CREATE FUNCTION reservas_empleado_periodo(empleado_id INT, fecha_inicio DATETIME, fecha_fin DATETIME) RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+	DECLARE reservas_count INT;
+    
+    SELECT COUNT(*) INTO reservas_count
+    FROM Reserva
+    WHERE empleado_id = idEmpleado
+    AND FechaReserva >= fecha_inicio
+    AND FechaReserva <= fecha_fin;
+    
+    RETURN reservas_count;
+END //
+
+DELIMITER ;
