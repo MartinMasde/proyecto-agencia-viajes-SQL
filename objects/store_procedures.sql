cffc2d2af036301e69ebef0d19a170f08b33b7a3
Martin Mas de Ayala <132627081+MartinMasde@users.noreply.github.com>
2024-07-20 19:45:09 -0300

    Update store_procedures.sql

--- objects/store_procedures.sql
+++ objects/store_procedures.sql
@@ -1,39 +1,75 @@
 USE agencia_viajes;
 
 DROP PROCEDURE IF EXISTS sp_nueva_reserva;
+DROP PROCEDURE IF EXISTS sp_agregar_cliente;
 
 DELIMITER //
 
 CREATE PROCEDURE sp_nueva_reserva(
-    IN p_fechaViaje DATETIME,
-    IN p_idCliente INT,
-    IN p_idViaje INT,
-    IN p_idEmpleado INT
+	IN p_fechaViaje DATETIME,
+	IN p_idCliente INT,
+	IN p_idViaje INT,
+	IN p_idEmpleado INT
 )
 BEGIN
-  DECLARE existeCliente INT;
-  DECLARE existeEmpleado INT;
+	DECLARE existeCliente INT;
+	DECLARE existeEmpleado INT;
     
-    -- Verificar si existe el Cliente
-  SELECT COUNT(*) INTO existeCliente
-  FROM Cliente
-  WHERE idCliente = p_idCliente;
-    
+	-- Verificar si existe el Cliente
+	SELECT COUNT(*) INTO existeCliente
+	FROM Cliente
+	WHERE idCliente = p_idCliente;
+
+	-- Verificar si existe el Empleado
 	SELECT COUNT(*) INTO existeEmpleado
-  FROM Empleado
-  WHERE idEmpleado = p_idEmpleado;
+	FROM Empleado
+	WHERE idEmpleado = p_idEmpleado;
     
-  IF existeCliente = 0 THEN
-	-- Si no existe el cliente, devolver un mensaje de error
-    SELECT 'El idCliente ingresado no existe';
+	IF existeCliente = 0 THEN
+    		-- Si no existe el cliente, devolver un mensaje de error
+    		SELECT 'El idCliente ingresado no existe';
 	ELSEIF existeEmpleado = 0 THEN
+		-- Si no existe el empleado, devolver un mensaje de error
 		SELECT 'El idEmpleado ingresado no existe';
 	ELSE 
 		INSERT INTO Reserva (FechaReserva, FechaViaje, idCliente, idViaje, idEmpleado)
 		VALUES (NOW(), p_fechaViaje, p_idCliente, p_idViaje, p_idEmpleado);
         
-    -- Devuelvo el ID de la reserva creada
-		SELECT LAST_INSERT_ID() AS ID_Reserva;
+                -- Devuelvo el ID de la reserva creada
+		SELECT 'Reserva creada exitosamente, su idReserva es: ', LAST_INSERT_ID() AS id_Reserva_Nueva;
+	END IF;
+END //
+
+DELIMITER ;
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE sp_agregar_cliente (
+	IN p_Nombre VARCHAR(100),
+	IN p_Apellido VARCHAR(100),
+	IN p_Direccion VARCHAR(200),
+	IN p_Telefono VARCHAR(100),
+	IN p_Email VARCHAR(100),
+	IN p_TipoCliente VARCHAR(10)
+)
+BEGIN
+	DECLARE existeCliente INT;
+
+	-- Verifico si ya existe el cliente a traves de su Email
+	SELECT COUNT(*) INTO existeCliente
+	FROM Cliente
+	WHERE Email = p_Email;
+    
+	IF existeCliente = 1 THEN
+		-- Si existe el cliente, devolver un mensaje de error
+		SELECT 'El cliente con el Email ', p_Email, ' ya se encuentra registrado en la Base de Datos.';
+   	ELSE 
+		INSERT INTO Cliente (Nombre, Apellido, Direccion, Telefono, Email, TipoCliente)
+		VALUES (p_Nombre, p_Apellido, p_Direccion, p_Telefono, p_Email, p_TipoCliente);
+		-- Devuelvo el ID de la reserva creada
+		SELECT 'Cliente creado exitosamente, su idCliente es: ', LAST_INSERT_ID() AS id_cliente_nuevo;
 	END IF;
 END //
 
