cbbf7a3151f78a0790e7cad7f883f2720dd9e6c9
Martin Mas de Ayala <132627081+MartinMasde@users.noreply.github.com>
2024-07-26 20:15:31 -0300

    Update triggers.sql
    Creacion de trigger trg_after_update_reserva

--- objects/triggers.sql
+++ objects/triggers.sql
@@ -1,1 +1,56 @@
+USE agencia_viajes;
 
+DROP TRIGGER IF EXISTS trg_after_update_reserva;
+
+-- Crear la tabla Log_Auditoria para guardar los cambios realizados en las tablas de la BD
+CREATE TABLE Log_Auditoria (
+    idLog INT AUTO_INCREMENT,
+    NombreTabla VARCHAR(100),
+    NombreColumna VARCHAR(100),
+    ValorAnterior VARCHAR(255),
+    ValorNuevo VARCHAR(255),
+    Usuario VARCHAR(100),
+    FechaCambio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (idLog)
+);
+
+-- Trigger para registrar cambios (UPDATE) en la tabla Reserva y respaldar los cambios en la tabla Log_Auditoria
+DELIMITER //
+
+CREATE TRIGGER trg_after_update_reserva
+AFTER UPDATE ON Reserva
+FOR EACH ROW
+BEGIN
+    DECLARE colName VARCHAR(100);
+    DECLARE oldValue VARCHAR(255);
+    DECLARE newValue VARCHAR(255);
+    
+    -- Verificar y registrar cambios en FechaReserva
+    IF OLD.FechaReserva <> NEW.FechaReserva THEN
+        SET colName = 'FechaReserva';
+        SET oldValue = OLD.FechaReserva;
+        SET newValue = NEW.FechaReserva;
+        INSERT INTO Log_Auditoria (NombreTabla, NombreColumna, ValorAnterior, ValorNuevo, Usuario, FechaCambio)
+        VALUES ('Reserva', colName, oldValue, newValue, CURRENT_USER(), NOW());
+    END IF;
+
+    -- Verificar y registrar cambios en FechaViaje
+    IF OLD.FechaViaje <> NEW.FechaViaje THEN
+        SET colName = 'FechaViaje';
+        SET oldValue = OLD.FechaViaje;
+        SET newValue = NEW.FechaViaje;
+        INSERT INTO Log_Auditoria (NombreTabla, NombreColumna, ValorAnterior, ValorNuevo, Usuario, FechaCambio)
+        VALUES ('Reserva', colName, oldValue, newValue, CURRENT_USER(), NOW());
+    END IF;
+
+    -- Verificar y registrar cambios en idViaje
+    IF OLD.idViaje <> NEW.idViaje THEN
+        SET colName = 'idViaje';
+        SET oldValue = OLD.idViaje;
+        SET newValue = NEW.idViaje;
+        INSERT INTO Log_Auditoria (NombreTabla, NombreColumna, ValorAnterior, ValorNuevo, Usuario, FechaCambio)
+        VALUES ('Reserva', colName, oldValue, newValue, CURRENT_USER(), NOW());
+    END IF;
+END //
+
+DELIMITER ;
